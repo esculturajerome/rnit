437482d3058866878c60e2c2541651e25f095649
Jerome Escultura <jerome.scultura@mecca.com.au>
2023-02-10 23:20:47 +0800

    tested form

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,6 @@ export default function Document() {
           name="keywords"
           content="RNIT, TESDA, ROMBLON, RNIT TESDA, TESDA ROMBLON"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="stylesheet"
           type="text/css"
