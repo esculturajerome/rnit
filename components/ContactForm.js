a7c9ff8b4ccb08532039ff21e6253b3abbb9946f
Jerome Escultura <jerome.scultura@mecca.com.au>
2023-02-10 10:16:16 +0800

    Added test page

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,6 +1,6 @@
 const ContactForm = () => {
   return (
-    <div>
+    <div id="contact">
       <form
         data-netlify="true"
         method="post"
---
+++ pages/testpage.js
@@ -0,0 +1,62 @@
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import Head from "next/head";
+
+function TestPage() {
+  return (
+    <>
+      <Head>
+        <title>RNIT | Test Page</title>
+        <meta
+          name="description"
+          content="RNIT page with Team Information and Organizational Chart"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+      </Head>
+      <Nav />
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+
+      <Footer />
+    </>
+  );
+}
+
+export default About;
