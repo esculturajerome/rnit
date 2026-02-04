4d3e5eeb90e8cf19db8e777970e8df45db2ce8b2
Jerome <esculturajerome@protonmail.com>
2026-02-04 13:44:29 +0800

    update landing page

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -8,12 +8,10 @@ export default function ProgramsPage() {
     return (
         <>
             <FeaturePrograms
-                maxPrograms={PROGRAMS_DATA.length} // Display all programs
                 showTitleRow={true} // Let FeaturePrograms handle its title section
                 title="Our Programs"
                 subText="Explore the diverse range of technical and vocational programs offered at RNIT."
                 showViewAllButton={false} // Not needed as we are on the "all programs" page
-                displayMode="uniformTwoColumn" // Use the new two-column layout
             />
             <AccreditedProgramsTable
                 utprasData={UTPRASData}
